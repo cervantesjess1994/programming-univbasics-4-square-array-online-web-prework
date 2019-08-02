810b30f446af932f3ce0c1272c85b0d01b22bc09
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-02 07:19:53 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+def square_array
+  puts array.sqrt
+end
+
+counter = 0
+array = [1,2,3]
+
+while array[counter] do
+  puts array[counter]
+  counter.sqrt
 end
